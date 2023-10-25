fd79f62567041e3c413b0e27a424963863be03d3
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-10-24 20:03:31 -0600

    Inicio

---
+++ src/main/java/com/mycompany/practica4/TestDispositivo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica4;
+
+/**
+ *
+ * @author nenod
+ */
+public class TestDispositivo {
+	public static void main(String[] args){
+		Laptop lap = new Laptop();
+		lap.encender();
+		lap.verCargaBateria();
+		lap.apagar();
+	}
+}
